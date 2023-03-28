c13d4609432e2ce2ea80212ca8946f86d64d13a5
Freya Arthen <50927803+fdarthen@users.noreply.github.com>
2023-03-28 10:41:55 +0200

    v0.7.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as input:
 
 setup(
     name="taxaminer",
-    version="0.7.0",
+    version="0.7.1",
     python_requires='>=3.7.0',
     description="Interactive exploration of biodiverse genome assemblies",
     long_description=long_description,
